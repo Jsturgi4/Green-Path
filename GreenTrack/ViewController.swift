0fc2d1683c926ddb2efa053ef33a87a1b4f916b5
Randy Martinez <randysaid0424@gmail.com>
2022-10-31 18:55:58 -0400

    initial Commit

--- GreenTrack/ViewController.swift
+++ GreenTrack/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button: UIButton = {
+        let but = UIButton(type: .close)
+        but.translatesAutoresizingMaskIntoConstraints = false
+        return but
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addSubview(button)
+        
     }
-
-
+    
+    
+            
+    func createButton () {
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 60 , y: 360, width: 240, height: 40)
+        button.backgroundColor = .red
+        button.setTitle("Hello", for: .normal)
+        self.view.addSubview(button)
+    }
 }
 
